54cb91342998e8b37eb778cfefa123671fa6d6a9
Piyal Shah <piyalsha007@gmail.com>
2025-04-23 12:45:24 +0600

    fix double loading animation at doctor details page

--- src/Pages/DoctorDetails.jsx
+++ src/Pages/DoctorDetails.jsx
@@ -32,9 +32,8 @@ const DoctorDetails = () => {
 
     if (loading) {
         return (
-            <div className="min-h-screen flex flex-col items-center justify-center gap-4">
-                <div className="w-16 h-16 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
-                <p className="text-gray-600">Loading doctor details...</p>
+            <div className="fixed inset-0 backdrop-blur-md bg-black/30 z-50 flex items-center justify-center">
+                <span className="loading loading-bars loading-xl text-blue-600"></span>
             </div>
         );
     }
